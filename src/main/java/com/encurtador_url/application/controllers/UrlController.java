cf12f20c9419a3601a73c9c432aa7cb771b5ef42
jjunior112 <juarezgcjr@gmail.com>
2025-09-02 01:34:22 -0300

    feat: controller para urls

---
+++ src/main/java/com/encurtador_url/application/controllers/UrlController.java
@@ -0,0 +1,46 @@
+package com.encurtador_url.application.controllers;
+
+import com.encurtador_url.application.services.UrlService;
+import com.encurtador_url.domain.dtos.UrlRequestDto;
+import com.encurtador_url.domain.dtos.UrlResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+public class UrlController {
+    private final UrlService urlService;
+    private final HttpServletRequest httpServletRequest;
+
+    public UrlController(UrlService urlService, HttpServletRequest httpServletRequest) {
+        this.urlService = urlService;
+        this.httpServletRequest = httpServletRequest;
+    }
+
+    @PostMapping(value = "/shorten-url")
+    public ResponseEntity<UrlResponseDto> generateShortenUrl(@RequestBody UrlRequestDto requestDto) {
+        var result = urlService.generate(requestDto.url());
+
+        String redirectUrl = httpServletRequest.getRequestURL().toString().replace("shorten-url", result);
+
+        return ResponseEntity.ok(new UrlResponseDto(redirectUrl));
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Void> getUrl(@PathVariable String id) {
+
+        var url = urlService.getUrl(id);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setLocation(URI.create(url));
+
+        return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+    }
+
+}
